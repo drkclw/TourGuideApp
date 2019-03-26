c5ac1b4a23c91df7b30c1af88a32ef50d484301c
Sam Gomez <Sgomez@hmbnet.com>
2019-03-26 12:41:23 -0400

    Added Autofac to the solution.

---
+++ TourGuideApp/TourGuideApp.iOS/IOSModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using Foundation;
+using UIKit;
+
+namespace TourGuideApp.iOS
+{
+    public class IOSModule : Module
+    {
+    }
+}
--- TourGuideApp/TourGuideApp/App.xaml.cs
+++ TourGuideApp/TourGuideApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Autofac;
+using System;
+using TourGuideApp.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,11 +9,13 @@ namespace TourGuideApp
 {
     public partial class App : Application
     {
-        public App()
+        public IContainer Container { get; }
+
+        public App(Module module)
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            Container = BuildContainer(module);
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
@@ -27,6 +31,14 @@ namespace TourGuideApp
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        IContainer BuildContainer(Module module)
+        {
+            var builder = new ContainerBuilder();            
+            builder.RegisterType<NavigationService>().AsSelf().SingleInstance();
+            builder.RegisterModule(module);
+            return builder.Build();
         }
     }
 }
