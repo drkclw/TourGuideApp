45daef03dfbe7fc01ca098942a309ac188a4f22c
Sam Gomez <Sgomez@hmbnet.com>
2019-03-26 11:43:08 -0400

    Add BaseModel and NavigationService for pages. XSD for FodyWeaver.xml

---
+++ TourGuideApp/TourGuideApp/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TourGuideApp.Models
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool IsBusy { get; protected set; }
+    }
+}
